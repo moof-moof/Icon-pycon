be49f832e45120576603de16bf70339bfba7be63
Loxia labs <bergman.martin@gmail.com>
2021-01-22 12:28:15 +0100

    Update SAVE_desktop_layout.py

--- icon-pycon/home/<USERNAME>/Desktop/DTbkp.d/1-SAVING/SAVE_desktop_layout.py
+++ icon-pycon/home/<USERNAME>/Desktop/DTbkp.d/1-SAVING/SAVE_desktop_layout.py
@@ -1,8 +1,8 @@
 
 
 import sys
-# sys.path.append('/home/user/path/to/0-defs/')
-sys.path.append('/home/xneb/Skrivbord/DTbkp.d/0-defs/')
+sys.path.append('/home/user/path/to/0-defs/')
+# sys.path.append('/home/xneb/Skrivbord/DTbkp.d/0-defs/')
 
 import icons_pycons
 
