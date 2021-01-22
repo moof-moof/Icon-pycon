e568f075df5613075588f567dfb187d6c27e92c6
Loxia labs <bergman.martin@gmail.com>
2021-01-22 03:39:47 +0100

    Add files via upload

---
+++ icon-pycon/home/<USERNAME>/Desktop/DTbkp.d/2-RESTORING/RESTORE_desktop_layout.py
@@ -0,0 +1,27 @@
+
+ 
+import sys
+# sys.path.append('/home/user/path/to/0-defs/')
+sys.path.append('/home/xneb/Skrivbord/DTbkp.d/0-defs/')
+
+import icons_pycons
+
+
+
+
+icons_pycons.RESTORE_saved_layout()
+
+
+
+
+# ---------------------------------------------------------------------#
+##   Finally, to refresh the desktop simply hit F5 while "touching" the
+##   desktop with the pointer.
+##
+##   To more drastically restart caja run in separate terminal: 
+##      caja -q && caja -n &
+##
+##   or:
+##      killall -9 caja (Oy Gevald!)
+# ---------------------------------------------------------------------#
+
